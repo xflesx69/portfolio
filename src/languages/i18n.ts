1fc329e8f4719333897210d7365a8504402bd4c6
xflesx69 <116289929+xflesx69@users.noreply.github.com>
2024-07-22 15:29:17 +0200

    lang fix

--- src/languages/i18n.ts
+++ src/languages/i18n.ts
@@ -2,17 +2,37 @@ import { ref } from 'vue';
 import en from '@/languages/en';
 import hu from '@/languages/hu';
 
-const currentLocale = ref('en');
-const messages = {
+interface LanguageMessages {
+  greeting: string;
+  home: {
+    myname: string;
+    ima: string;
+    desc: string;
+    desc2: string;
+  };
+  footer: {
+    english: string;
+    hungarian: string;
+  };
+}
+
+interface Messages {
+  en: LanguageMessages;
+  hu: LanguageMessages;
+}
+
+const currentLocale = ref<'en' | 'hu'>('en');
+
+const messages: Messages = {
   en,
   hu
 };
 
 const t = (key: string): string => {
-  return key.split('.').reduce((o, i) => (o as any)[i], messages[currentLocale.value]);
+  return key.split('.').reduce((o, i) => (o as any)[i], messages[currentLocale.value] as any);
 };
 
-const changeLanguage = (language: string) => {
+const changeLanguage = (language: 'en' | 'hu') => {
   currentLocale.value = language;
 };
 
